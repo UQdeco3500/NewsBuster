f9bef8be37568d463424fee409ebca08c041d363
Kevin <kevinkao1126@gmail.com>
2023-10-02 21:08:02 +1000

    Add detection page

---
+++ frontend/link.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementsByClassName('hover_img-l')[0].addEventListener('click', function() {
+    window.location.href = 'map.html';
+});
+
+document.getElementsByClassName('hover_img-r')[0].addEventListener('click', function() {
+    window.location.href = 'detect.html';
+});
