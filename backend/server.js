cf0bf714bcc3c62954d0a8986f08b843adea1bf6
Kevin <kevinkao1126@gmail.com>
2023-10-19 09:38:56 +1000

    docker file

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,27 @@
 
-const multer = require('multer');
 const express = require('express');
 const path = require("path");
 const app = express();
 
-// Define storage for uploaded files
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Uploads will be stored in the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, 'instance.png'); // Set unique filenames
-  },
-});
-
-const upload = multer({ storage: storage });
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../frontend"));
-app.use(express.static('../frontend'))
+app.set("views", path.join(__dirname, "./frontend"));
+app.use(express.static('./frontend'))
 
-app.get('/detect', (req, res) => {
-  res.render('detect.ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello, Docker!');
+})
+
+app.get('/map', (req, res) => {
+  res.render('map.html');
 })
 
 // Handle file upload
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log('Image uploaded successfully');
-});
+// app.post('/upload', upload.single('image'), (req, res) => {
+//   console.log('Image uploaded successfully');
+// });
 
 
 app.listen(8080, () => {
