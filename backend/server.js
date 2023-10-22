8888c839091c5c401b35a8f49564153e1a1f8468
Kevin <kevinkao1126@gmail.com>
2023-10-22 15:18:04 +1000

    add SQL code and socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,88 @@
 
-const multer = require('multer');
 const express = require('express');
 const path = require("path");
+const mysql = require('mysql2');
+const socketIo = require('socket.io');
+
 const app = express();
 
-// Define storage for uploaded files
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Uploads will be stored in the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, 'instance.png'); // Set unique filenames
-  },
-});
+const http = require('http').Server(app);
 
-const upload = multer({ storage: storage });
+const io = socketIo(http);
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../frontend"));
-app.use(express.static('../frontend'))
+app.set("views", path.join(__dirname, "./frontend"));
+app.use(express.static('./frontend'))
 
-app.get('/detect', (req, res) => {
-  res.render('detect.ejs');
-})
+const db = mysql.createConnection({
+  host: 'localhost',
+  port: 8889,
+  user: 'root',
+  password: 'root',
+  database: 'social_mobile',
+});
 
-// Handle file upload
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log('Image uploaded successfully');
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to the database');
+  }
 });
 
 
-app.listen(8080, () => {
-    console.log("LISTENING ON PORT 8080");
+
+io.on('connection', (socket) => {
+  console.log('A client has connected');
+
+  // Handle the database data emission here
+  socket.on('requestDatabaseData', () => {
+    // Query the database and emit the data
+    db.query('SELECT * from arts', (err, results) => {
+      if (!err) {
+        // Emit the data to the specific client
+        console.log(results)
+        socket.emit('databaseData', results);
+      }
+    });
   });
+
+
+  socket.on('userData',userData=>{
+
+    db.query('UPDATE arts SET user_count = ?, width = ? WHERE id = ?', [userData.user_count + 1, userData.width + 1, userData.id], (err, results) => {
+      if (err) {
+        console.error('Error updating data:', err);
+      } else {
+        // Emit an event to notify connected clients about the data update
+        console.log('Upload!!!')
+      }
+    });
+
+  });
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, Docker!');
+})
+
+app.get('/map', (req, res) => {
+  res.render('map.html');
+})
+
+// Handle file upload
+// app.post('/upload', upload.single('image'), (req, res) => {
+//   console.log('Image uploaded successfully');
+// });
+
+
+// app.listen(8080, () => {
+//     console.log("LISTENING ON PORT 8080");
+//   });
   
-  
+http.listen(8080, () => console.log(`Listening on port ${8080}`));
